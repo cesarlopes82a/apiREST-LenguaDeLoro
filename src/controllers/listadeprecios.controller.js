7840aad431998c3f7888ee907b4f59c5f7a33580
Cesar Lopez <cesarlopez82a@gmail.com>
2022-09-28 11:11:22 -0300

    Revert "fix 413 Request Entity Too Large"
    This reverts commit e2c51b9eba20a26268234530b3aeb84ddb8c1d5e.

--- src/controllers/listadeprecios.controller.js
+++ src/controllers/listadeprecios.controller.js
@@ -75,7 +75,8 @@ export const getListasdpByStoreIdAndPopulateInfo = async(req, res) => {
    
     
   if(listaFound){   
-    
+
+    console.log(listaFound)
     console.log("MENSAJE: listas de precios encontradas para " + storeId);
     return res.status(200).json(listaFound)
   }else{
@@ -104,7 +105,8 @@ export const registrarNuevaLDP = async(req,res) => {
 
   let arrayProductosOfLDP = []
   for (let producto of params.products) {
-
+    console.log("esto es un produccccccccc")
+    console.log(producto)
     let objldpproduct = {
       product: producto._id,
       precioVenta: producto.precioVenta
